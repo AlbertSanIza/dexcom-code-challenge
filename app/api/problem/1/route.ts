8b4113b4d53939513817daa2e4a0d747fb1f8658
Albert Sanchez <albert.san.iza@gmail.com>
2023-07-23 16:51:48 -0700

    Create handlers

--- app/api/problem/1/route.ts
+++ app/api/problem/1/route.ts
@@ -1,17 +1,6 @@
 import { NextResponse } from 'next/server'
 
-function solution(nums: number[]): number[] {
-    const numsSet = new Set(nums)
-
-    const missingNumbers: number[] = []
-    for (let i = 1; i <= nums.length; i++) {
-        if (!numsSet.has(i)) {
-            missingNumbers.push(i)
-        }
-    }
-
-    return missingNumbers
-}
+import solution from '@/utils/solution-1'
 
 export async function POST(request: Request) {
     const body = await request.json()
--- app/api/problem/2/route.ts
+++ app/api/problem/2/route.ts
@@ -1,14 +1,6 @@
 import { NextResponse } from 'next/server'
 
-function solution(nums: number[], target: number): number[] {
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] + nums[i + 1] === target) {
-            return [i, i + 1]
-        }
-    }
-
-    return []
-}
+import solution from '@/utils/solution-2'
 
 export async function POST(request: Request) {
     const body = await request.json()
