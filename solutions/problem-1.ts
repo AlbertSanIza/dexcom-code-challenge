c61738d429a6ae50ccee6fcd99d61656252d00d4
Albert Sanchez <albert.san.iza@gmail.com>
2023-07-23 20:00:11 -0700

    Add comments to solutions

--- solutions/problem-1.ts
+++ solutions/problem-1.ts
@@ -1,10 +1,17 @@
 export default function solution(nums: number[]): number[] {
+    // Transform the nums array into a set to eliminate duplicates
     const numsSet = new Set(nums)
+
+    // Create an array to store the missing numbers
     const missingNumbers: number[] = []
+
+    // Iterate through the numbers 1 to nums.length
     for (let i = 1; i <= nums.length; i++) {
+        // If the number does not exist in the set, add it to the missing numbers array
         if (!numsSet.has(i)) {
             missingNumbers.push(i)
         }
     }
+
     return missingNumbers
 }
--- solutions/problem-2.ts
+++ solutions/problem-2.ts
@@ -1,8 +1,12 @@
 export default function solution(nums: number[], target: number): number[] {
+    // Iterate through nums array
     for (let i = 0; i < nums.length - 1; i++) {
+        // If the current number plus the next number equals the target, return the indices of the numbers
         if (nums[i] + nums[i + 1] === target) {
             return [i, i + 1]
         }
     }
+
+    // If no pair of numbers equals the target, return an empty array
     return []
 }
