1cc6d495812622e0939f93ef4af944d14868ed07
Albert Sanchez <albert.san.iza@gmail.com>
2023-07-23 18:13:30 -0700

    Update Code.tsx

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { CodeBlock } from 'react-code-blocks'
 
-export default function Button({ text }: { text: string }) {
-    return <CodeBlock language="typescript" text={text} startingLineNumber={1} wrapLongLines={false} />
+export default function Button({ text, language = 'typescript' }: { text: string; language?: string }) {
+    return <CodeBlock language={language} text={text} startingLineNumber={1} wrapLongLines={false} />
 }
