db5d114e3f04402a6d147a5cb30fabe8616024cb
Albert Sanchez <albert.san.iza@gmail.com>
2023-07-23 17:03:55 -0700

    rename to getSolutionString

--- utils/getSolutionString.ts
+++ utils/getSolutionString.ts
@@ -1,5 +1,5 @@
 import { readFile } from 'fs/promises'
 
 export default async function getSolutionString(problemNumber: number): Promise<string> {
-    return await readFile(`./app/api/problem/1/${problemNumber}.ts`, 'utf-8')
+    return await readFile(`./solutions/problem-${problemNumber}.ts`, 'utf-8')
 }
